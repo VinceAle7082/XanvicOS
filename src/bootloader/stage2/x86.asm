1344e39d1469c2d6c2d4723d5f4d608bc553c06b
Vincenzo Aleksey Brocato <vinceale7082@gmail.com>
2024-07-28 00:08:48 +0200

    Now the Stage 1 of the bootloader loads Stage 2 in memory. I already wrote this but my computer crashed, so I won't write as much as before. Now the bootloader is divided in stage1 and stage2, I added and edited some makefiles, stage 2 is written in Asm and C, the kernel isn't loaded into memory.
    PS: after this commit I'll add another branch called "coding", the code in that branch won't work 100% because it's missing something, but I don't want to do a "mega commit" every time (those commits will still be in the main branch).
    To everyone who wants to try XanvicOS or """using""" it, use the main branch, because the main branch's code is working.
    I'll write this in the README.md right after this commit.

---
+++ src/bootloader/stage2/x86.asm
@@ -0,0 +1,23 @@
+bits 16
+
+section _TEXT class=CODE
+
+global _x86_Video_WriteCharTeletype
+
+_x86_Video_WriteCharTeletype:
+    push bp
+    mov bp, sp
+
+    push bx
+    mov ah, 0Eh
+    mov al, [bp + 4]
+    mov bh, [bp + 6]
+
+    int 10h
+
+    pop bx
+    
+    mov sp, bp
+    pop bp
+
+    ret
