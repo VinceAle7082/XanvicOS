49f191d0166da11e007d19cab97dc887b241f31e
VinceAle7082 <vinceale7082@gmail.com>
2024-09-10 18:50:49 +0200

    boh

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -5,7 +5,7 @@ void _cdecl cstart_(uint16_t bootDrive)
 {
     const char far* far_str = "far string";
 
-    puts("Hello World from Stage2!");
+    puts("Hello World from Stage 2!\r\n");
     printf("Hello World from printf!");
     for (;;);
 
